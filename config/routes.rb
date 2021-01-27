1521e27dd5c438a5dfecadbe51e5c4c51dbed73d
yuuki-abc <yuki.abc.300@gmail.com>
2021-01-27 16:44:18 +0000

    [Finish] 3-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'blogs/index'
-  get 'blogs/show'
-  get 'blogs/new'
-  get 'blogs/edit'
+
+  # get 'blogs/index'
+  # get 'blogs/show'
+  # get 'blogs/new'
+  # get 'blogs/edit'
+
+  resources :blogs
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
