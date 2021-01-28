b1661190ac141d7cd0f4d025c34784434ddcdfea
yuuki-abc <yuki.abc.300@gmail.com>
2021-01-28 13:22:36 +0000

    [Start] 3-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,7 @@
 class BlogsController < ApplicationController
   
   def show
+    @blog ＝Blog.find(params[:id])
   end
   
   def index
@@ -14,7 +15,7 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blog_path(blog.id)
   end
 
   def edit
